617ac3b58c3fa49a62174f255fc98f1efbcd9c25
Vamshi Surabhi <vamshi@34cross.in>
2016-03-09 16:53:31 +0530

    Move to the new syntax of update

--- hasura-db/src/main/java/io/hasura/db/UpdateQuery.java
+++ hasura-db/src/main/java/io/hasura/db/UpdateQuery.java
@@ -26,14 +26,14 @@ public class UpdateQuery<R> extends QueryWithReturning<UpdateQuery<R>, R> {
         new GsonBuilder()
         .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
         .create();
-    private JsonObject updObj;
+    private JsonObject setObj;
     private JsonObject whereExp;
     private DBService db;
     private Table<R> table;
 
     public UpdateQuery(DBService db, Table<R> table) {
         super();
-        this.updObj = new JsonObject();
+        this.setObj = new JsonObject();
         this.whereExp = null;
         this.table = table;
         this.db = db;
@@ -46,7 +46,7 @@ public class UpdateQuery<R> extends QueryWithReturning<UpdateQuery<R>, R> {
 
     public <T> UpdateQuery<R> set(PGField<R, T> fld, T val) {
         Type valType = new TypeToken<T>(){}.getType();
-        this.updObj.add(fld.getColumnName(), gson.toJsonTree(val, valType));
+        this.setObj.add(fld.getColumnName(), gson.toJsonTree(val, valType));
         return this;
     }
 
@@ -64,7 +64,7 @@ public class UpdateQuery<R> extends QueryWithReturning<UpdateQuery<R>, R> {
     public Call<UpdateResult<R>, DBException> build() {
         /* Create the query object */
         JsonObject query = new JsonObject();
-        query.add("values", this.updObj);
+        query.add("$set", this.setObj);
         if (this.whereExp != null)
             query.add("where", this.whereExp);
 
