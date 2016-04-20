d98dcadbafe3a1c527642a8602c3bec972c7f1ef
Tanmai Gopal <tanmaig@34cross.in>
2016-04-20 18:44:41 +0530

    Adds session_id response to SocialLoginResponse

--- hasura-auth/src/main/java/io/hasura/auth/SocialLoginResponse.java
+++ hasura-auth/src/main/java/io/hasura/auth/SocialLoginResponse.java
@@ -13,6 +13,9 @@ public class SocialLoginResponse {
     @SerializedName("new_user")
     boolean newUser;
 
+    @SerializedName("session_id")
+    String sessionId;
+
     @SerializedName("info")
     JsonObject info;
 
@@ -22,6 +25,10 @@ public class SocialLoginResponse {
 
     public String getHasuraRole() {
         return hasuraRole;
+    }
+
+    public String getSessionId() {
+        return sessionId;
     }
 
     public boolean isNewUser() {
